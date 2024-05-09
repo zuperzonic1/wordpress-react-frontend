12b648416b21a5d3ddbd3c8dc4f3848abc1ae074
Micheal Felobes <mickelsteam1@gmail.com>
2024-05-09 17:10:19 -0400

    removed default darkmode from navbar, as Images are not setup for dark mode.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,7 +12,7 @@ export default function Navigation() {
   };
 
   return (
-    <nav className="bg-white border border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 shadow">
+    <nav className="bg-white border border-gray-200  px-2 sm:px-4 py-2.5 rounded shadow">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link to="/" className="flex items-center">
           <img src="/logo.png" alt="Logo" className="h-8 sm:h-10" /> {/* Adjust image size as needed */}
@@ -22,7 +22,7 @@ export default function Navigation() {
           <button
             id="menu-toggle"
             type="button"
-            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200  md:hidden"
             onClick={toggleMenu}
           >
             <span className="sr-only">Open main menu</span>
@@ -42,7 +42,7 @@ export default function Navigation() {
             <li>
               <Link
                 to="/"
-                className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 "
                 aria-current="page"
               >
                 Home
@@ -51,7 +51,7 @@ export default function Navigation() {
             <li>
               <Link
                 to="/articles"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
               >
                 Articles
               </Link>
