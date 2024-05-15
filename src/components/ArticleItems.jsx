5974a1af8765d5ef8fc41911876b866b3b0ceaf8
Micheal Felobes <mickelsteam1@gmail.com>
2024-05-15 12:13:19 -0400

    fixed loading page

--- src/components/ArticleItems.jsx
+++ src/components/ArticleItems.jsx
@@ -5,10 +5,12 @@ function ArticleItems({ isLoaded, articles }) {
     return (
       <div className="flex justify-center items-center h-screen">
         <div
-          className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
           role="status"
         >
-          <span className="visually-hidden">Loading...</span>
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
         </div>
       </div>
     );
