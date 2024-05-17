524cf1bf1f99cb1250e55d801ab196e93752b0bc
Micheal Felobes <mickelsteam1@gmail.com>
2024-05-17 08:34:41 -0400

    changing footer info to be generic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,37 @@
-// Grabbed the foter form here, and made some changes to it to make it fit the react project.
-// https://tailwindcomponents.com/component/footer-5
-
 function Footer() {
   return (
     <footer className="relative bg-white pt-8 pb-6">
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap text-left lg:text-left">
           <div className="w-full lg:w-6/12 px-4">
-            <h4 className="text-3xl font-semibold text-blueGray-700">
-              Let's keep in touch!
+            <h4 className="text-3xl font-semibold text-gray-700">
+              FRT MOTTO (Not created yet)
             </h4>
-            <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
-              Find us on any of these platforms, we respond 1-2 business days.
+            <h5 className="text-lg mt-0 mb-2 text-gray-600">
+              Learn more about Facial recognition technology and how its effects
+              on privacy.
             </h5>
             <div className="mt-6 lg:mb-0 mb-6">
               <button
-                className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                className="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                 type="button"
               >
                 <i className="fab">icon</i>
               </button>
               <button
-                className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                className="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                 type="button"
               >
                 <i className="fab">icon</i>
               </button>
               <button
-                className="bg-white text-lightblue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                className="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                 type="button"
               >
                 <i className="fab">icon</i>
               </button>
               <button
-                className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                className="bg-white text-gray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                 type="button"
               >
                 <i className="fab">icon</i>
@@ -43,77 +41,77 @@ function Footer() {
           <div className="w-full lg:w-6/12 px-4">
             <div className="flex flex-wrap items-top mb-6">
               <div className="w-full lg:w-4/12 px-4 ml-auto">
-                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                <span className="block uppercase text-gray-500 text-sm font-semibold mb-2">
                   Useful Links
                 </span>
                 <ul className="list-unstyled">
                   <li>
                     <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="/"
+                      className="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm"
+                      href="#!"
                     >
                       About Us
                     </a>
                   </li>
                   <li>
                     <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="/"
+                      className="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm"
+                      href="#!"
                     >
                       Blog
                     </a>
                   </li>
                   <li>
                     <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="/"
+                      className="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm"
+                      href="#!"
                     >
-                      Github
+                      Projects
                     </a>
                   </li>
                   <li>
                     <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="/"
+                      className="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm"
+                      href="#!"
                     >
-                      Free Products
+                      Resources
                     </a>
                   </li>
                 </ul>
               </div>
               <div className="w-full lg:w-4/12 px-4">
-                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                <span className="block uppercase text-gray-500 text-sm font-semibold mb-2">
                   Other Resources
                 </span>
                 <ul className="list-unstyled">
                   <li>
                     <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="/"
+                      className="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm"
+                      href="#!"
                     >
-                      MIT License
+                      Support
                     </a>
                   </li>
                   <li>
                     <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="/"
+                      className="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm"
+                      href="#!"
                     >
                       Terms & Conditions
                     </a>
                   </li>
                   <li>
                     <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="/"
+                      className="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm"
+                      href="#!"
                     >
                       Privacy Policy
                     </a>
                   </li>
                   <li>
                     <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="/"
+                      className="text-gray-600 hover:text-gray-800 font-semibold block pb-2 text-sm"
+                      href="#!"
                     >
                       Contact Us
                     </a>
@@ -123,25 +121,14 @@ function Footer() {
             </div>
           </div>
         </div>
-        <hr className="my-6 border-blueGray-300" />
+        <hr className="my-6 border-gray-300" />
         <div className="flex flex-wrap items-center md:justify-between justify-center">
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-            <div className="text-sm text-blueGray-500 font-semibold py-1">
-              Copyright ©2024
-              <a
-                href="/"
-                className="text-blueGray-500 hover:text-gray-800"
-                target="_blank"
-              >
+            <div className="text-sm text-gray-500 font-semibold py-1">
+              Copyright © 2024
+              <a href="#!" className="text-gray-500 hover:text-gray-800">
                 {" "}
-                Notus JS by
-                <a
-                  href="/"
-                  className="text-blueGray-500 hover:text-blueGray-800"
-                >
-                  Creative Tim
-                </a>
-                .
+                Your Company Name{" "}
               </a>
             </div>
           </div>
